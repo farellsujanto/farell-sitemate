bc99b0261676e4080a26c4766c8dcc6a28ea89bb
farellsujanto <f.sujanto@gmail.com>
2025-01-11 22:21:58 +0700

    Add PrimaryInput component

---
+++ src/components/inputs/PrimaryInput/index.tsx
@@ -0,0 +1,23 @@
+import React, { useCallback } from 'react';
+import { TextInput } from 'react-native';
+import styles from './styles';
+
+interface PrimaryInputProps {
+    placeholder?: string;
+    onChangeText: (text: string) => void;
+};
+
+const PrimaryInput = ({
+    placeholder,
+    onChangeText,
+}: PrimaryInputProps) => {
+
+    return (
+        <TextInput
+            style={styles.primaryInput}
+            placeholder={placeholder}
+            onChangeText={onChangeText} />
+    );
+};
+
+export default React.memo(PrimaryInput);
---
+++ src/components/inputs/PrimaryInput/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    primaryInput: {
+        height: 40,
+        width: '100%',
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 12,
+    },
+});
